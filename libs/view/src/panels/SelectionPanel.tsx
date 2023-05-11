488f4847ac6a9279cb4b75ec37f1d32d587f4b58
Shota Matsuda <shota@takram.com>
2023-05-12 06:32:55 +0900

    Remove selection ID list

--- libs/view/src/panels/SelectionPanel.tsx
+++ libs/view/src/panels/SelectionPanel.tsx
@@ -1,4 +1,3 @@
-import { Cesium3DTileFeature } from '@cesium/engine'
 import { Stack, Typography, styled } from '@mui/material'
 import { useAtomValue } from 'jotai'
 import { useContext, type FC } from 'react'
@@ -22,16 +21,6 @@ export const SelectionPanel: FC = () => {
         <Typography variant='subtitle2'>
           Selection Count: {selection.length}
         </Typography>
-        {selection.map((selection, index) => {
-          if (selection instanceof Cesium3DTileFeature) {
-            return (
-              <Typography key={index} variant='body2'>
-                {selection.getProperty('_gml_id')}
-              </Typography>
-            )
-          }
-          return null
-        })}
       </Stack>
     </Root>
   )
