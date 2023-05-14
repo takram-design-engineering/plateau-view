dd896ef0c7d720cb29b560cd7269f14543e38b58
Shota Matsuda <shota@takram.com>
2023-05-15 05:01:40 +0900

    Add references to parents and children

--- apps/data-converters/src/targets/municipalityCodes.ts
+++ apps/data-converters/src/targets/municipalityCodes.ts
@@ -1,46 +1,97 @@
 import { readFile, writeFile } from 'fs/promises'
-import { fromPairs } from 'lodash'
+import { fromPairs, isArray } from 'lodash'
 import path from 'path'
+import invariant from 'tiny-invariant'
 
-import { isNotFalse } from '@plateau/type-helpers'
+type Prefectures = Record<
+  string, // prefectureCode
+  string // prefectureName
+>
+
+type Municipalities = Record<
+  string, // municipalityName
+  | string // municipalityCode
+  | [string, string[]] // [municipalityCode, childCodes]
+  | [string, string] // [municipalityCode, parentCode]
+>
 
 // Get the data at: https://www.e-stat.go.jp/municipalities/cities/areacode
-const source = 'estat/FEA_hyoujun-20230507084049.csv'
+const source = path.resolve('./data/estat/FEA_hyoujun-20230507084049.csv')
+
+function addChild(
+  childCode: string,
+  parentCode: string,
+  record: Municipalities
+): void {
+  const parent = record[parentCode]
+  invariant(parent != null)
+  if (typeof parent === 'string') {
+    record[parentCode] = [parent, [childCode]]
+  } else {
+    invariant(isArray(parent[1]))
+    parent[1].push(childCode)
+  }
+}
 
 export async function main(): Promise<void> {
   const { csvParseRows } = await import('d3')
-  const prefectures: Record<string, string> = {}
-  const municipalities = csvParseRows(
-    await readFile(path.resolve('./data', source), 'utf-8'),
-    (
-      [municipalityCode, prefectureName, cityName, , municipalityName],
-      index
-    ) => {
-      if (index === 0) {
-        return
-      }
-      const prefectureCode = municipalityCode.slice(0, 2)
-      if (prefectures[prefectureName] == null) {
-        prefectures[prefectureCode] = prefectureName
-      }
-      return [
-        municipalityCode,
-        [
-          cityName.endsWith('市') && cityName,
-          municipalityName !== '' && municipalityName
+
+  const designatedCityCodes: Record<string, string> = {}
+  const prefectures: Prefectures = {}
+  const municipalities: Municipalities = fromPairs(
+    csvParseRows(
+      await readFile(source, 'utf-8'),
+      (
+        [municipalityCode, prefectureName, cityName, , municipalityName],
+        index
+      ) => {
+        if (index === 0) {
+          return
+        }
+        const prefectureCode = municipalityCode.slice(0, 2)
+        if (prefectures[prefectureName] == null) {
+          prefectures[prefectureCode] = prefectureName
+        }
+
+        // Remember designated cities.
+        if (cityName.endsWith('市') && municipalityName === '') {
+          designatedCityCodes[cityName] = municipalityCode
+        }
+        // Append the parent code for designated city wards, with exception of
+        // Tokyo 23 wards.
+        if (municipalityCode === '13100') {
+          return [municipalityCode, '東京23区']
+        }
+        if (/^131\d{2}$/.test(municipalityCode)) {
+          return [municipalityCode, [municipalityName, '13100']]
+        }
+        if (cityName.endsWith('市') && municipalityName !== '') {
+          const parentCode = designatedCityCodes[cityName]
+          invariant(parentCode != null)
+          return [municipalityCode, [municipalityName, parentCode]]
+        }
+        return [
+          municipalityCode,
+          municipalityName === '' ? cityName : municipalityName
         ]
-          .filter(isNotFalse)
-          .join(' ')
-      ]
-    }
+      }
+    )
   )
+
+  Object.entries(municipalities)
+    .filter((entry): entry is [string, [string, string]] =>
+      Array.isArray(entry[1])
+    )
+    .forEach(([municipalityCode, [, parentCode]]) => {
+      addChild(municipalityCode, parentCode, municipalities)
+    })
 
   const output = {
     prefectures,
-    municipalities: fromPairs(municipalities)
+    municipalities
   }
   await writeFile(
-    path.resolve('./data/municipalities.json'),
+    path.resolve('./data/municipalityCodes.json'),
     JSON.stringify(output)
   )
   console.log('Done')
