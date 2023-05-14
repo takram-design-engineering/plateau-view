dbdb936ac654ba857fa35acb6c6b4f1dee59d9a1
Shota Matsuda <shota@takram.com>
2023-05-14 20:51:22 +0900

    Add version to building dataset variants

--- apps/api/src/plateau/dto/PlateauBuildingDataset.ts
+++ apps/api/src/plateau/dto/PlateauBuildingDataset.ts
@@ -6,6 +6,9 @@ import { PlateauDataset, type PlateauDatasetType } from './PlateauDataset'
 
 @ObjectType()
 class PlateauBuildingDatasetVariant {
+  @Field()
+  version!: string
+
   @Field(() => Number)
   lod!: number
 
@@ -20,51 +23,63 @@ class PlateauBuildingDatasetVariant {
   implements: [PlateauDataset]
 })
 export class PlateauBuildingDataset extends PlateauDataset<PlateauDatasetType.Building> {
-  @Field(() => [PlateauBuildingDatasetVariant])
-  get variants(): PlateauBuildingDatasetVariant[] {
-    const version = this.version ?? '2020' // Prefer 2020
-
-    if (version === '2022') {
-      // Strangely, when a LOD doesn't have a variant with non-textured suffix,
-      // the only variant of the LOD is *not* textured, which forces me to
-      // traverse all the variants first.
-      const groups = groupBy(
-        this.catalog.data.config.data.map(variant => ({
-          lod: +variant.name.slice(3, 4),
-          variant
-        })),
-        'lod'
-      )
-      return Object.values(groups).flatMap(variants =>
-        variants.map(({ lod, variant }) => ({
+  private getVariants2022(): PlateauBuildingDatasetVariant[] {
+    // Strangely, when a LOD doesn't have a variant with non-textured suffix,
+    // the only variant of the LOD is *not* textured, which forces me to
+    // traverse all the variants first.
+    const groups = groupBy(
+      this.catalog.data.config.data.map(variant => ({
+        lod: +variant.name.slice(3, 4),
+        variant
+      })),
+      'lod'
+    )
+    return Object.values(groups).flatMap(variants =>
+      variants
+        .map(({ lod, variant }) => ({
+          version: '2022',
           lod,
           textured:
             variants.length > 1 && !variant.name.includes('テクスチャなし'),
           url: variant.url
         }))
-      )
-    }
+        .sort((a, b) =>
+          `${a.lod}-${a.textured ? 1 : 0}`.localeCompare(
+            `${b.lod}-${b.textured ? 1 : 0}}`
+          )
+        )
+    )
+  }
 
-    if (version === '2020') {
-      // Frustratingly, when there are no files with "low_resolution" suffix,
-      // the LOD of the files *without* "low_resolution" suffix is 1, otherwise
-      // the LOD of the files *with* "low_resolution" suffix is 1. This also
-      // forces me to traverse all the variants first.
-      const files = this.storageService.match({
-        pattern: format('01_building/{code}_*_2020_bldg_*/tileset.json', {
-          code: this.catalog.data.ward_code ?? this.catalog.data.city_code
-        }),
+  private getVariants2020(): PlateauBuildingDatasetVariant[] {
+    // Frustratingly, when there are no files with "low_resolution" suffix, the
+    // LOD of the files *without* "low_resolution" suffix is 1, otherwise the
+    // LOD of the files *with* "low_resolution" suffix is 1. This also forces me
+    // to traverse all the variants first.
+    const files = this.storageService.match({
+      pattern: format('01_building/{code}_*_2020_bldg_*/tileset.json', {
+        code: this.catalog.data.ward_code ?? this.catalog.data.city_code
+      }),
+      version: '2020',
+      fileType: '3dtiles'
+    })
+    const lod = files.some(file => file.includes('_low_resolution')) ? 2 : 1
+    return files
+      .map(file => ({
         version: '2020',
-        fileType: '3dtiles'
-      })
-      const lod = files.some(file => file.includes('_low_resolution')) ? 2 : 1
-      return files.map(file => ({
         lod: file.includes('_low_resolution') ? 1 : lod,
         textured: file.includes('_texture'),
         url: file
       }))
-    }
+      .sort((a, b) =>
+        `${a.lod}-${a.textured ? 1 : 0}`.localeCompare(
+          `${b.lod}-${b.textured ? 1 : 0}}`
+        )
+      )
+  }
 
-    return []
+  @Field(() => [PlateauBuildingDatasetVariant])
+  get variants(): PlateauBuildingDatasetVariant[] {
+    return [...this.getVariants2020(), ...this.getVariants2022()]
   }
 }
