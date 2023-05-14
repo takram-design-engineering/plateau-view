7ecc15acb84cb7e1c1686ef874d5b6ae030dc46b
Shota Matsuda <shota@takram.com>
2023-05-15 05:01:22 +0900

    Add areas to municipality polygons

--- apps/data-converters/src/targets/municipalityPolygons.ts
+++ apps/data-converters/src/targets/municipalityPolygons.ts
@@ -1,6 +1,6 @@
-import { writeFile } from 'fs/promises'
 import { type FeatureCollection } from 'geojson'
 import { runCommands } from 'mapshaper'
+import { mkdirp } from 'mkdirp'
 import path from 'path'
 import { read as readShapefile } from 'shapefile'
 import invariant from 'tiny-invariant'
@@ -12,6 +12,7 @@ interface Properties {
   municipalityName: string
   prefectureCode: string
   prefectureName: string
+  area: number
 }
 
 // Convert shapefile into GeoJSON, preprocessing geometries and properties.
@@ -30,11 +31,15 @@ async function convertToGeoJSON(file: string): Promise<FeatureCollection> {
         if (typeof input.CITY !== 'string' || typeof input.PREF !== 'string') {
           return undefined
         }
+        if (isNaN(+input.AREA)) {
+          throw new Error('Cannot coarse area to number')
+        }
         const properties: Properties = {
           municipalityCode: `${input.PREF}${input.CITY}`,
           municipalityName: input.CITY_NAME,
           prefectureCode: input.PREF,
-          prefectureName: input.PREF_NAME
+          prefectureName: input.PREF_NAME,
+          area: input.AREA
         }
         invariant(feature.geometry.type === 'Polygon')
         return {
@@ -60,7 +65,7 @@ async function writeTopoJSON(
       `-simplify interval=${interval}`,
       // Merge geometries of the same municipality code, copying properties of
       // the first geometry.
-      '-dissolve fields=municipalityCode copy-fields=municipalityName,prefectureCode,prefectureName',
+      '-dissolve fields=municipalityCode copy-fields=municipalityName,prefectureCode,prefectureName sum-fields=area',
       // Remove useless geometries.
       '-clean',
       `-o "${file}" format=topojson`
@@ -71,24 +76,24 @@ async function writeTopoJSON(
   )
 }
 
-// Get the data at:
-// https://www.e-stat.go.jp/gis/statmap-search?page=1&type=2&aggregateUnitForBoundary=A&toukeiCode=00200521&toukeiYear=2020&serveyId=B002005212020&prefCode=01&coordsys=1&format=shape&datum=2000
-const sources = [...Array(47)].map((_, index) => {
-  const code = `${index + 1}`.padStart(2, '0')
-  return `estat/A002005212015DDSWC${code}-JGD2011/h27ka${code}`
-})
-
 export async function main(): Promise<void> {
-  for (const source of sources) {
-    const geojson = await convertToGeoJSON(path.resolve('./data', source))
-    await writeFile(
-      path.resolve('./data/estat', `${source}.geojson`),
-      JSON.stringify(geojson)
+  const prefectureCodes = [...Array(47)].map((_, index) =>
+    `${index + 1}`.padStart(2, '0')
+  )
+  await mkdirp(path.resolve('./data/municipalityPolygons'))
+  for (const prefectureCode of prefectureCodes) {
+    // Get the data at:
+    // https://www.e-stat.go.jp/gis/statmap-search?page=1&type=2&aggregateUnitForBoundary=A&toukeiCode=00200521&toukeiYear=2020&serveyId=B002005212020&prefCode=01&coordsys=1&format=shape&datum=2000
+    const geojson = await convertToGeoJSON(
+      path.resolve(
+        `./data/estat/A002005212015DDSWC${prefectureCode}-JGD2011/h27ka${prefectureCode}`
+      )
     )
     await writeTopoJSON(
-      path.resolve('./data/estat', `${source}.topojson`),
+      path.resolve('./data/municipalityPolygons', `${prefectureCode}.topojson`),
       geojson
     )
-    console.log('Done')
+    console.log(`Saved ${prefectureCode}.topojson`)
   }
+  console.log('Done')
 }
