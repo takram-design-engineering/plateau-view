ea846fc802f15474e333574d12d1b8d3de19abcb
Shota Matsuda <shota@takram.com>
2023-05-15 10:04:21 +0900

    Include prefecture geometry and rename to area polygons and codes

--- apps/data-converters/src/main.ts
+++ apps/data-converters/src/main.ts
@@ -3,22 +3,32 @@ import minimist from 'minimist'
 import path from 'path'
 import invariant from 'tiny-invariant'
 
+async function printTargets(targetDir: string): Promise<void> {
+  console.log('Available targets:')
+  ;(await readdir(targetDir))
+    .filter(name => name.endsWith('.js'))
+    .forEach(name => {
+      console.log(`  - ${path.parse(name).name}`)
+    })
+}
+
 async function main(): Promise<void> {
   const argv = minimist(process.argv.slice(2))
   const targetDir = path.resolve(__dirname, 'targets')
   if (argv.target == null) {
-    console.log('Available targets:')
-    ;(await readdir(targetDir))
-      .filter(name => name.endsWith('.js'))
-      .forEach(name => {
-        console.log(`  - ${path.parse(name).name}`)
-      })
+    await printTargets(targetDir)
     process.exit(0)
   }
 
   const importPath = path.join(targetDir, argv.target)
   invariant(importPath.startsWith(targetDir))
-  const target = await import(`${importPath}.js`)
+  let target
+  try {
+    target = await import(`${importPath}.js`)
+  } catch (error) {
+    await printTargets(targetDir)
+    process.exit(1)
+  }
   await target.main(argv)
 }
 
