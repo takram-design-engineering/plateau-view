6283546e7224a72e9cb3e6ec28e31367f62b2deb
Shota Matsuda <shota@takram.com>
2023-05-18 02:45:03 +0900

    Remove near-useless prefetch because parameters are packed in JWT

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -4,7 +4,6 @@ import { useHydrateAtoms } from 'jotai/utils'
 import { type GetServerSideProps, type NextPage } from 'next'
 import Head from 'next/head'
 import { Suspense, type FC } from 'react'
-import invariant from 'tiny-invariant'
 import UAParser from 'ua-parser-js'
 
 import { LoadingScreen, type Platform } from '@plateau/ui-components'
@@ -21,20 +20,10 @@ interface PageProps {
 
 const Page: NextPage<PageProps> = ({ platform }) => {
   useHydrateAtoms([[platformAtom, platform]])
-  invariant(
-    process.env.NEXT_PUBLIC_PLATEAU_TERRAIN_ACCESS_TOKEN != null,
-    'Missing environment variable: NEXT_PUBLIC_PLATEAU_TERRAIN_ACCESS_TOKEN'
-  )
   return (
     <>
       <Head>
         <title>PLATEAU VIEW 3.0 Design & Technology Preview</title>
-        <link
-          rel='preload'
-          as='fetch'
-          crossOrigin='anonymous'
-          href={`https://api.cesium.com/v1/assets/770371/endpoint?access_token=${process.env.NEXT_PUBLIC_PLATEAU_TERRAIN_ACCESS_TOKEN}`}
-        />
       </Head>
       <Suspense>
         <PlateauView />
