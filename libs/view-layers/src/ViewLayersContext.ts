068ce1dcc98c8993c4055cd6a6e2f4b8e677be7f
Shota Matsuda <shota@takram.com>
2023-05-20 12:44:50 +0900

    Add ViewLayersContext

---
+++ libs/view-layers/src/ViewLayersContext.ts
@@ -0,0 +1,13 @@
+import { atom } from 'jotai'
+import { createContext } from 'react'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function createContextValue() {
+  return {
+    pixelRatioAtom: atom(1)
+  }
+}
+
+export type ViewLayersContextValue = ReturnType<typeof createContextValue>
+
+export const ViewLayersContext = createContext(createContextValue())
