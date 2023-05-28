207528a33e10bf85730f5a398f04ed6bcf0bf7bd
Shota Matsuda <shota@takram.com>
2023-05-29 03:39:36 +0900

    Add selection groups in app state

--- libs/view/src/states/selection.ts
+++ libs/view/src/states/selection.ts
@@ -1,13 +1,16 @@
 import { atom } from 'jotai'
+import { groupBy } from 'lodash'
 
 import {
   layerSelectionAtom,
   layersAtom,
-  type LayerModel
+  type LayerModel,
+  type LayerType
 } from '@takram/plateau-layers'
 import {
   screenSpaceSelectionAtom,
-  type ScreenSpaceSelectionEntry
+  type ScreenSpaceSelectionEntry,
+  type ScreenSpaceSelectionType
 } from '@takram/plateau-screen-space-selection'
 import { isNotNullish } from '@takram/plateau-type-helpers'
 
@@ -24,7 +27,9 @@ export type Selection =
       value: ScreenSpaceSelectionEntry
     }
 
-export const selectionAtom = atom(get => [
+export type SelectionType = Selection['type']
+
+export const selectionAtom = atom((get): Selection[] => [
   ...get(layerSelectionAtom)
     .map((id): (Selection & { type: typeof LAYER_SELECTION }) | undefined => {
       const layer = get(layersAtom).find(layer => layer.id === id)
@@ -46,3 +51,59 @@ export const selectionAtom = atom(get => [
     })
   )
 ])
+
+export type SelectionGroup =
+  | ({
+      type: typeof LAYER_SELECTION
+    } & {
+      [K in LayerType]: {
+        subtype: K
+        values: Array<LayerModel<K>>
+      }
+    }[LayerType])
+  | ({
+      type: typeof SCREEN_SPACE_SELECTION
+    } & {
+      [K in ScreenSpaceSelectionType]: {
+        subtype: K
+        values: Array<ScreenSpaceSelectionEntry<K>['value']>
+      }
+    }[ScreenSpaceSelectionType])
+
+export const selectionGroupsAtom = atom<SelectionGroup[]>(get => {
+  const groups = Object.entries(
+    groupBy(get(selectionAtom), 'type')
+  ) as unknown as Array<
+    {
+      [K in SelectionType]: [K, Array<Selection & { type: K }>]
+    }[SelectionType]
+  >
+  return groups.flatMap(([type, values]) => {
+    switch (type) {
+      case LAYER_SELECTION:
+        return Object.entries(
+          groupBy(
+            values.map(({ value }) => value),
+            'type'
+          )
+        ).map(([subtype, values]) => ({
+          type,
+          subtype,
+          values
+        })) as unknown as SelectionGroup[]
+      case SCREEN_SPACE_SELECTION: {
+        return Object.entries(
+          groupBy(
+            values.map(({ value }) => value),
+            'type'
+          )
+        ).map(([subtype, values]) => ({
+          type,
+          subtype,
+          values: values.map(({ value }) => value)
+        })) as unknown as SelectionGroup[]
+      }
+    }
+    return []
+  })
+})
