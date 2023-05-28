cb397fb4d08fa3c07c806b54094c6fb32485ae9d
Shota Matsuda <shota@takram.com>
2023-05-29 03:39:47 +0900

    Add feature selection state in datasets

--- libs/datasets/src/states.ts
+++ libs/datasets/src/states.ts
@@ -1,5 +1,20 @@
+import { atom } from 'jotai'
 import { atomWithReset } from 'jotai/utils'
+
+import {
+  screenSpaceSelectionAtom,
+  type ScreenSpaceSelectionEntry
+} from '@takram/plateau-screen-space-selection'
+
+import { PLATEAU_TILESET } from './PlateauTileset'
 
 export const showTilesetTextureAtom = atomWithReset(true)
 export const showTilesetWireframeAtom = atomWithReset(false)
 export const showTilesetBoundingVolumeAtom = atomWithReset(false)
+
+export const featureSelectionAtom = atom(get => {
+  return get(screenSpaceSelectionAtom).filter(
+    (entry): entry is ScreenSpaceSelectionEntry<typeof PLATEAU_TILESET> =>
+      entry.type === PLATEAU_TILESET
+  )
+})
