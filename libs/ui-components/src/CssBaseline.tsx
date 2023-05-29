3588cbd453833da0910da21b0cc708662648e5e2
Shota Matsuda <shota@takram.com>
2023-05-30 03:53:01 +0900

    Fix that font size is bigger in Chrome

--- libs/ui-components/src/CssBaseline.tsx
+++ libs/ui-components/src/CssBaseline.tsx
@@ -15,6 +15,13 @@ export const CssBaseline: FC = () => {
       <MuiCssBaseline />
       <GlobalStyles
         styles={css`
+          html {
+            // "lang=ja" automatically sets this but it makes fonts little
+            // bigger in Chrome. I don't know whether it's Chrome's behavior or
+            // some libraries are injecting styles.
+            -webkit-locale: auto;
+          }
+
           body {
             font-family: ${theme.typography.fontFamily};
             letter-spacing: 0.015em;
