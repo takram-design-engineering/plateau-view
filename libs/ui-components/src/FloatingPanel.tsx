8eaf1c2c40d8222f81b433a8bc60ee436658e96b
Shota Matsuda <shota@takram.com>
2023-05-30 06:10:12 +0900

    Separate Scrollable from FloatingPanel

--- libs/ui-components/src/FloatingPanel.tsx
+++ libs/ui-components/src/FloatingPanel.tsx
@@ -1,11 +1,7 @@
 import { Paper, styled, type PaperProps } from '@mui/material'
-import {
-  useOverlayScrollbars,
-  type UseOverlayScrollbarsParams
-} from 'overlayscrollbars-react'
-import { forwardRef, useEffect, useRef, type ReactNode } from 'react'
-import { mergeRefs } from 'react-merge-refs'
-import invariant from 'tiny-invariant'
+import { forwardRef } from 'react'
+
+import { Scrollable } from './Scrollable'
 
 const StyledPaper = styled(Paper)(({ theme, elevation = 4 }) => ({
   position: 'relative',
@@ -26,23 +22,16 @@ const StyledPaper = styled(Paper)(({ theme, elevation = 4 }) => ({
 const RoundedBox = styled('div')(({ theme }) => ({
   overflow: 'hidden',
   maxHeight: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  minHeight: 0,
   borderRadius: theme.shape.borderRadius
 }))
 
-const ScrollableRoundedBox = forwardRef<
-  HTMLDivElement,
-  UseOverlayScrollbarsParams & {
-    children?: ReactNode
-  }
->(({ options, events, defer, ...props }, forwardedRef) => {
-  const ref = useRef<HTMLDivElement>(null)
-  const [initialize] = useOverlayScrollbars({ options, events, defer })
-  useEffect(() => {
-    invariant(ref.current != null)
-    initialize(ref.current)
-  }, [initialize])
-  return <RoundedBox ref={mergeRefs([ref, forwardedRef])} {...props} />
-})
+const ScrollableRoundedBox = styled(Scrollable)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius
+}))
 
 export interface FloatingPanelProps extends PaperProps {
   scrollable?: boolean
---
+++ libs/ui-components/src/Scrollable.tsx
@@ -0,0 +1,36 @@
+import { styled } from '@mui/material'
+import {
+  useOverlayScrollbars,
+  type UseOverlayScrollbarsParams
+} from 'overlayscrollbars-react'
+import {
+  forwardRef,
+  useEffect,
+  useRef,
+  type ComponentPropsWithRef,
+  type ReactNode
+} from 'react'
+import { mergeRefs } from 'react-merge-refs'
+import invariant from 'tiny-invariant'
+
+const Root = styled('div')({
+  maxHeight: '100%'
+})
+
+export interface ScrollableProps
+  extends ComponentPropsWithRef<typeof Root>,
+    UseOverlayScrollbarsParams {
+  children?: ReactNode
+}
+
+export const Scrollable = forwardRef<HTMLDivElement, ScrollableProps>(
+  ({ options, events, defer, ...props }, forwardedRef) => {
+    const ref = useRef<HTMLDivElement>(null)
+    const [initialize] = useOverlayScrollbars({ options, events, defer })
+    useEffect(() => {
+      invariant(ref.current != null)
+      initialize(ref.current)
+    }, [initialize])
+    return <Root ref={mergeRefs([ref, forwardedRef])} {...props} />
+  }
+)
