d3fc69ba9b65fd41697c1f37bc369d2e865a84e5
Shota Matsuda <shota@takram.com>
2023-05-30 05:04:28 +0900

    Add expanded prop in icon

--- libs/ui-components/src/icons/ExpandArrowIcon.tsx
+++ libs/ui-components/src/icons/ExpandArrowIcon.tsx
@@ -1,6 +1,19 @@
-import { createSvgIcon } from '@mui/material'
+import { createSvgIcon, styled } from '@mui/material'
 
-export const ExpandArrowIcon = createSvgIcon(
-  <path d='M11.9995 15.0384L6.3457 9.38464L7.39953 8.33081L11.9995 12.9308L16.5995 8.33081L17.6534 9.38464L11.9995 15.0384Z' />,
+const Icon = createSvgIcon(
+  <path d='M11.9995 16.0384L6.3457 10.38464L7.39953 9.33081L11.9995 13.9308L16.5995 9.33081L17.6534 10.38464L11.9995 16.0384Z' />,
   'ExpandArrow'
 )
+
+export const ExpandArrowIcon = styled(Icon, {
+  shouldForwardProp: prop => prop !== 'expanded'
+})<{
+  expanded?: boolean
+}>(({ theme, expanded = false }) => ({
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shorter
+  }),
+  ...(expanded && {
+    transform: 'rotate(180deg)'
+  })
+}))
