a1e0650173237010eebcd343a3f7623124358d56
Shota Matsuda <shota@takram.com>
2023-06-01 07:44:34 +0900

    Fix environment variable

--- apps/tiles/webpack.config.js
+++ apps/tiles/webpack.config.js
@@ -2,6 +2,6 @@ const { composePlugins, withNx } = require('@nx/webpack')
 const webpack = require('webpack')
 
 module.exports = composePlugins(withNx(), config => {
-  config.plugins.push(new webpack.EnvironmentPlugin(['VECTOR_TILE_CACHE_ROOT']))
+  config.plugins.push(new webpack.EnvironmentPlugin(['TILE_CACHE_ROOT']))
   return config
 })
