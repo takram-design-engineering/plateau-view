0eaa46ff965a16abac8647a3500a2cbc129f9731
Shota Matsuda <shota@takram.com>
2023-05-31 13:30:02 +0900

    Fix warning

--- libs/color-schemes/src/ColorScheme.ts
+++ libs/color-schemes/src/ColorScheme.ts
@@ -1,4 +1,4 @@
-import { interpolate, quantize, scaleLinear, type ScaleLinear } from 'd3'
+import { interpolate, quantize, rgb, scaleLinear, type ScaleLinear } from 'd3'
 import invariant from 'tiny-invariant'
 
 import { type ColorTuple, type LUT } from './types'
@@ -26,5 +26,18 @@ export class ColorScheme {
     return [...Array(count)].map((_, index) => {
       return this.linear(index / (count - 1))
     })
+  }
+
+  createImage(): HTMLCanvasElement {
+    const canvas = document.createElement('canvas')
+    canvas.width = this.lut.length
+    canvas.height = 1
+    const context = canvas.getContext('2d')
+    invariant(context != null)
+    this.lut.forEach(([r, g, b], index) => {
+      context.fillStyle = rgb(r * 0xff, g * 0xff, b * 0xff).toString()
+      context.fillRect(index, 0, 1, 1)
+    })
+    return canvas
   }
 }
