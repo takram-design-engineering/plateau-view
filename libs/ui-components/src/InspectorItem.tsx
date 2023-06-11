03b8204ad494204728969cd0a20bc56d445265b4
Shota Matsuda <shota@takram.com>
2023-06-11 19:38:51 +0900

    Add disablePadding prop

--- libs/ui-components/src/InspectorItem.tsx
+++ libs/ui-components/src/InspectorItem.tsx
@@ -1,14 +1,22 @@
 import { styled } from '@mui/material'
 import { type FC, type ReactNode } from 'react'
 
-const Root = styled('div')(({ theme }) => ({
-  padding: `${theme.spacing(1)} ${theme.spacing(2)}`
+const Root = styled('div', {
+  shouldForwardProp: prop => prop !== 'disablePadding'
+})<{
+  disablePadding?: boolean
+}>(({ theme, disablePadding = false }) => ({
+  ...(!disablePadding && {
+    padding: `${theme.spacing(1)} ${theme.spacing(2)}`
+  })
 }))
 
 export interface InspectorItemProps {
+  disablePadding?: boolean
   children?: ReactNode
 }
 
-export const InspectorItem: FC<InspectorItemProps> = ({ children }) => (
-  <Root>{children}</Root>
-)
+export const InspectorItem: FC<InspectorItemProps> = ({
+  disablePadding = false,
+  children
+}) => <Root disablePadding={disablePadding}>{children}</Root>
