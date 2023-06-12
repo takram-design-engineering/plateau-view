8258aefc41184972f8fd23c530d5dfaf4134dd63
Shota Matsuda <shota@takram.com>
2023-06-13 07:25:42 +0900

    Roll frustum

--- libs/pedestrian/src/StreetViewFrustum.tsx
+++ libs/pedestrian/src/StreetViewFrustum.tsx
@@ -14,7 +14,8 @@ import {
   Transforms
 } from '@cesium/engine'
 import { useTheme } from '@mui/material'
-import { useMemo, type FC } from 'react'
+import { animate, useSpring, useTransform } from 'framer-motion'
+import { useEffect, useMemo, type FC } from 'react'
 import invariant from 'tiny-invariant'
 
 import { useCesium, useInstance, usePreRender } from '@takram/plateau-cesium'
@@ -23,6 +24,11 @@ import { FrustumAppearance } from './FrustumAppearance'
 import { getPosition } from './getPosition'
 import { type HeadingPitch, type Location } from './types'
 
+const roll = Quaternion.fromHeadingPitchRoll(
+  new HeadingPitchRoll(0, 0, CesiumMath.PI_OVER_TWO)
+)
+const headingPitchRollScratch = new HeadingPitchRoll()
+
 function createQuaternionFromHeadingPitch(
   headingPitch: HeadingPitch,
   position: Cartesian3,
@@ -30,15 +36,18 @@ function createQuaternionFromHeadingPitch(
 ): Quaternion {
   const heading = CesiumMath.toRadians(headingPitch.heading)
   const pitch = CesiumMath.toRadians(headingPitch.pitch)
-  const headingPitchRoll = new HeadingPitchRoll(
-    heading + CesiumMath.PI_OVER_TWO,
-    CesiumMath.PI_OVER_TWO - pitch
-  )
-  return Transforms.headingPitchRollQuaternion(
-    position,
-    headingPitchRoll,
-    undefined,
-    undefined,
+  headingPitchRollScratch.heading = heading + Math.PI
+  headingPitchRollScratch.pitch = 0
+  headingPitchRollScratch.roll = -pitch
+  return Quaternion.multiply(
+    Transforms.headingPitchRollQuaternion(
+      position,
+      headingPitchRollScratch,
+      undefined,
+      undefined,
+      result
+    ),
+    roll,
     result
   )
 }
@@ -54,6 +63,40 @@ interface StreetViewFrustumProps {
 const cartesianScratch = new Cartesian3()
 const quaternionScratch = new Quaternion()
 
+const colorGeometryAttribute = new GeometryAttribute({
+  componentDatatype: ComponentDatatype.UNSIGNED_BYTE,
+  componentsPerAttribute: 3,
+  normalize: true,
+  values: new Uint8Array(
+    [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0xff, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0xff, 0, 0],
+      [0, 0, 0],
+      [0xff, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0xff, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ].flat()
+  )
+})
+
 export const StreetViewFrustum: FC<StreetViewFrustumProps> = ({
   location,
   headingPitch,
@@ -64,13 +107,14 @@ export const StreetViewFrustum: FC<StreetViewFrustumProps> = ({
   const geometryInstance = useMemo(() => {
     // TODO: Zoom to FOV translation doesn't look correct especially when
     // it's zoomed in.
+    // TODO: Change FOV by scale of model matrix.
     const fovX = Math.PI / Math.pow(2, zoom)
     const fovY = 2 * Math.atan(aspectRatio * Math.tan(fovX / 2))
     const geometry = FrustumGeometry.createGeometry(
       new FrustumGeometry({
         frustum: new PerspectiveFrustum({
           fov: aspectRatio > 1 ? fovX : fovY,
-          aspectRatio: 1 / aspectRatio,
+          aspectRatio,
           near: 0.01,
           far: length
         }),
@@ -80,39 +124,7 @@ export const StreetViewFrustum: FC<StreetViewFrustumProps> = ({
       })
     )
     invariant(geometry != null)
-    geometry.attributes.color = new GeometryAttribute({
-      componentDatatype: ComponentDatatype.UNSIGNED_BYTE,
-      componentsPerAttribute: 3,
-      normalize: true,
-      values: new Uint8Array(
-        [
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0xff, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0xff, 0, 0],
-          [0, 0, 0],
-          [0xff, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0xff, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0]
-        ].flat()
-      )
-    })
+    geometry.attributes.color = colorGeometryAttribute
     return new GeometryInstance({ geometry })
   }, [zoom, aspectRatio, length])
 
@@ -128,10 +140,6 @@ export const StreetViewFrustum: FC<StreetViewFrustumProps> = ({
         asynchronous: false,
         appearance: new FrustumAppearance({
           color: Color.fromCssColorString(theme.palette.primary.main)
-        }),
-        depthFailAppearance: new FrustumAppearance({
-          color: Color.fromCssColorString(theme.palette.primary.main),
-          alpha: 0.2
         })
       }),
     transferOwnership: (primitive, primitives) => {
@@ -145,7 +153,49 @@ export const StreetViewFrustum: FC<StreetViewFrustumProps> = ({
   const scene = useCesium(({ scene }) => scene)
   scene.requestRender()
 
+  const motionLongitude = useSpring(location.longitude)
+  const motionLatitude = useSpring(location.latitude)
+  const motionHeight = useSpring(location.height ?? 0)
+
+  useEffect(() => {
+    return animate(motionLongitude, location.longitude, {
+      type: 'tween',
+      ease: 'easeOut',
+      duration: 0.2
+    }).stop
+  }, [location.longitude, motionLongitude])
+  useEffect(() => {
+    return animate(motionLatitude, location.latitude, {
+      type: 'tween',
+      ease: 'easeOut',
+      duration: 0.2
+    }).stop
+  }, [location.latitude, motionLatitude])
+  useEffect(() => {
+    return animate(motionHeight, location.height ?? 0, {
+      type: 'tween',
+      ease: 'easeOut',
+      duration: 0.2
+    }).stop
+  }, [location.height, motionHeight])
+
+  const motionLocation = useTransform(
+    [motionLongitude, motionLatitude, motionHeight],
+    (values: number[]) => ({
+      longitude: values[0],
+      latitude: values[1],
+      height: values[2]
+    })
+  )
+
+  useEffect(() => {
+    return motionLocation.on('change', () => {
+      scene.requestRender()
+    })
+  }, [scene, motionLocation])
+
   usePreRender(() => {
+    const location = motionLocation.get()
     const position = getPosition(location, scene, cartesianScratch)
     const rotation = createQuaternionFromHeadingPitch(
       headingPitch,
