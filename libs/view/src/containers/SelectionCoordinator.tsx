5bd6bd88e132d822a65e34dd5ebe99b180da6862
Shota Matsuda <shota@takram.com>
2023-06-13 12:52:41 +0900

    Remove workaround

--- libs/view/src/containers/SelectionCoordinator.tsx
+++ libs/view/src/containers/SelectionCoordinator.tsx
@@ -1,14 +1,10 @@
-import { useAtom, useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
 import { type FC } from 'react'
 import { useIsomorphicLayoutEffect } from 'react-use'
 
-import { useCesium } from '@takram/plateau-cesium'
 import { layerSelectionAtom } from '@takram/plateau-layers'
 import { PEDESTRIAN_OBJECT } from '@takram/plateau-pedestrian'
-import { useElementEvent } from '@takram/plateau-react-helpers'
 import { screenSpaceSelectionAtom } from '@takram/plateau-screen-space-selection'
-
-import { toolAtom } from '../states/tool'
 
 function clearValue<T>(prevValue: readonly T[]): readonly T[] {
   return prevValue.length !== 0 ? [] : prevValue
@@ -25,8 +21,8 @@ export const SelectionCoordinator: FC = () => {
       const [selection] = screenSpaceSelection
       if (selection.type === PEDESTRIAN_OBJECT) {
         const layerId = selection.value.split(':')[1]
-        setScreenSpaceSelection(clearValue)
         setLayerSelection([layerId])
+        setScreenSpaceSelection(clearValue)
         return
       }
     }
@@ -40,23 +36,6 @@ export const SelectionCoordinator: FC = () => {
       setScreenSpaceSelection(clearValue)
     }
   }, [layerSelection, setLayerSelection, setScreenSpaceSelection])
-
-  // TODO: Bit of workaround.
-  // TODO: Deal with touch events.
-  const canvas = useCesium(({ canvas }) => canvas)
-  const tool = useAtomValue(toolAtom)
-  useElementEvent(canvas, 'pointerdown', (event: MouseEvent) => {
-    if (
-      tool === 'select' &&
-      event.button === 0 &&
-      !event.shiftKey &&
-      !event.altKey &&
-      !event.ctrlKey &&
-      !event.metaKey
-    ) {
-      setLayerSelection(clearValue)
-    }
-  })
 
   return null
 }
