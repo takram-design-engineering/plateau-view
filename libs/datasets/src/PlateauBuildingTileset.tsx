6f3287e195bc33709f90d9d5896b1642876533fd
Shota Matsuda <shota@takram.com>
2023-07-02 01:15:25 +0900

    Take evaluateColor function as color prop

--- libs/datasets/src/PlateauBuildingTileset.tsx
+++ libs/datasets/src/PlateauBuildingTileset.tsx
@@ -1,18 +1,32 @@
-import { type Cesium3DTileset } from '@cesium/engine'
-import { forwardRef } from 'react'
+import { type Cesium3DTileset, type Cesium3DTileStyle } from '@cesium/engine'
+import { forwardRef, useEffect, useRef } from 'react'
+import { mergeRefs } from 'react-merge-refs'
 
 import { PlateauTileset, type PlateauTilesetProps } from './PlateauTileset'
-import { useDefaultTileStyle } from './useDefaultTileStyle'
+import { useDefaultTileStyle, type EvaluateColor } from './useDefaultTileStyle'
 
 export interface PlateauBuildingTilesetProps extends PlateauTilesetProps {
-  color?: string
+  color?: string | EvaluateColor
   opacity?: number
+  style?: Cesium3DTileStyle
 }
 
 export const PlateauBuildingTileset = forwardRef<
   Cesium3DTileset,
   PlateauBuildingTilesetProps
->(({ color, opacity, ...props }, forwardedRef) => {
-  const style = useDefaultTileStyle({ color, opacity })
-  return <PlateauTileset ref={forwardedRef} {...props} style={style} />
+>(({ color, opacity, style, ...props }, forwardedRef) => {
+  const defaultStyle = useDefaultTileStyle({ color, opacity })
+
+  const ref = useRef<Cesium3DTileset>(null)
+  useEffect(() => {
+    ref.current?.makeStyleDirty()
+  }, [color, opacity])
+
+  return (
+    <PlateauTileset
+      ref={mergeRefs([ref, forwardedRef])}
+      {...props}
+      style={style ?? defaultStyle}
+    />
+  )
 })
--- libs/datasets/src/useDefaultTileStyle.ts
+++ libs/datasets/src/useDefaultTileStyle.ts
@@ -1,12 +1,21 @@
-import { Cesium3DTileStyle, type Cesium3DTileFeature } from '@cesium/engine'
+import {
+  Cesium3DTileStyle,
+  Color,
+  type Cesium3DTileFeature
+} from '@cesium/engine'
 import { useTheme } from '@mui/material'
 import { useAtomValue } from 'jotai'
-import { useMemo } from 'react'
+import { useMemo, useRef } from 'react'
 
 import { colorModeAtom } from '@takram/plateau-shared-states'
 
+export type EvaluateColor = (
+  feature: Cesium3DTileFeature,
+  result: Color
+) => Color
+
 export interface DefaultTileStyleParams {
-  color?: string
+  color?: string | EvaluateColor
   opacity?: number
 }
 
@@ -16,6 +25,10 @@ export function useDefaultTileStyle({
 }: DefaultTileStyleParams): Cesium3DTileStyle {
   const theme = useTheme()
   const colorMode = useAtomValue(colorModeAtom)
+
+  const ref = useRef({ color, opacity })
+  Object.assign(ref.current, { color, opacity })
+
   return useMemo(() => {
     return new Cesium3DTileStyle({
       show: {
@@ -24,21 +37,34 @@ export function useDefaultTileStyle({
         }
       },
       color: {
-        conditions: [
-          // eslint-disable-next-line no-template-curly-in-string
-          ['${selected}', `color("${theme.palette.primary.main}")`],
-          [
-            'true',
-            `color("${
-              color != null
-                ? color
-                : colorMode === 'light'
-                ? '#ffffff'
-                : '#444444'
-            }", ${opacity})`
-          ]
-        ]
+        evaluateColor: (feature: Cesium3DTileFeature, result: Color): Color => {
+          const { color, opacity } = ref.current
+          if (feature.getProperty('selected') === true) {
+            result = Color.fromCssColorString(
+              theme.palette.primary.main,
+              result
+            )
+          } else if (color == null) {
+            result = Color.fromCssColorString(
+              colorMode === 'light' ? '#ffffff' : '#444444',
+              result
+            )
+          } else if (typeof color === 'string') {
+            result = Color.fromCssColorString(color, result)
+          } else {
+            try {
+              result = color(feature, result)
+            } catch (error) {
+              console.error(error)
+              result = Color.fromCssColorString(
+                colorMode === 'light' ? '#ffffff' : '#444444',
+                result
+              )
+            }
+          }
+          return result.withAlpha(opacity, result)
+        }
       }
     })
-  }, [color, opacity, theme, colorMode])
+  }, [theme, colorMode])
 }
