385513dbe0c250d6e9f7f14c3c6a0ec2abec50c3
Shota Matsuda <shota@takram.com>
2023-07-12 01:52:55 +0900

    Adjust search field style

--- libs/ui-components/src/SearchField.tsx
+++ libs/ui-components/src/SearchField.tsx
@@ -1,17 +1,33 @@
-import { InputAdornment, TextField, type TextFieldProps } from '@mui/material'
+import {
+  InputAdornment,
+  styled,
+  TextField,
+  type TextFieldProps
+} from '@mui/material'
 import { forwardRef } from 'react'
 
 import { SearchIcon } from './icons'
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  minHeight: theme.spacing(6),
+  padding: `${theme.spacing(1)} ${theme.spacing(1.5)}`
+}))
 
 export type SearchFieldProps = TextFieldProps
 
 export const SearchField = forwardRef<HTMLInputElement, SearchFieldProps>(
   (props, ref) => {
     return (
-      <TextField
+      <StyledTextField
         ref={ref}
+        variant='standard'
+        fullWidth
         {...props}
         InputProps={{
+          disableUnderline: true,
           startAdornment: (
             <InputAdornment position='start'>
               <SearchIcon sx={{ fontSize: 26 }} />
