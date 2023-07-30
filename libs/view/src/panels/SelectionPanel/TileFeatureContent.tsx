e3aa22cf1699136e8e02f2508e1597ee6b442970
Shota Matsuda <shota@takram.com>
2023-07-31 03:40:56 +0900

    Remove useless ephemerality

--- libs/view/src/panels/SelectionPanel/TileFeatureContent.tsx
+++ libs/view/src/panels/SelectionPanel/TileFeatureContent.tsx
@@ -4,7 +4,6 @@ import { useCallback, useState, type FC } from 'react'
 
 import { type PLATEAU_TILE_FEATURE } from '@takram/plateau-datasets'
 import { layerSelectionAtom } from '@takram/plateau-layers'
-import { withEphemerality } from '@takram/plateau-react-helpers'
 import { screenSpaceSelectionAtom } from '@takram/plateau-screen-space-selection'
 import {
   BuildingIcon,
@@ -32,64 +31,60 @@ export interface TileFeatureContentProps {
   })['values']
 }
 
-export const TileFeatureContent: FC<TileFeatureContentProps> = withEphemerality(
-  null,
-  ['values'],
-  ({ values }) => {
-    const setSelection = useSetAtom(screenSpaceSelectionAtom)
-    const handleClose = useCallback(() => {
-      setSelection([])
-    }, [setSelection])
+export const TileFeatureContent: FC<TileFeatureContentProps> = ({ values }) => {
+  const setSelection = useSetAtom(screenSpaceSelectionAtom)
+  const handleClose = useCallback(() => {
+    setSelection([])
+  }, [setSelection])
 
-    const [hidden, setHidden] = useState(false)
-    const hideFeatures = useSetAtom(hideFeaturesAtom)
-    const showFeatures = useSetAtom(showFeaturesAtom)
-    const handleHide = useCallback(() => {
-      hideFeatures(values.map(value => value.key))
-      setHidden(true)
-    }, [values, hideFeatures])
-    const handleShow = useCallback(() => {
-      showFeatures(values.map(value => value.key))
-      setHidden(false)
-    }, [values, showFeatures])
+  const [hidden, setHidden] = useState(false)
+  const hideFeatures = useSetAtom(hideFeaturesAtom)
+  const showFeatures = useSetAtom(showFeaturesAtom)
+  const handleHide = useCallback(() => {
+    hideFeatures(values.map(value => value.key))
+    setHidden(true)
+  }, [values, hideFeatures])
+  const handleShow = useCallback(() => {
+    showFeatures(values.map(value => value.key))
+    setHidden(false)
+  }, [values, showFeatures])
 
-    const tilesetLayers = useAtomValue(highlightedTilesetLayersAtom)
-    const setLayerSelection = useSetAtom(layerSelectionAtom)
-    const handleSelectLayers = useCallback(() => {
-      setLayerSelection(tilesetLayers.map(layer => layer.id))
-    }, [tilesetLayers, setLayerSelection])
+  const tilesetLayers = useAtomValue(highlightedTilesetLayersAtom)
+  const setLayerSelection = useSetAtom(layerSelectionAtom)
+  const handleSelectLayers = useCallback(() => {
+    setLayerSelection(tilesetLayers.map(layer => layer.id))
+  }, [tilesetLayers, setLayerSelection])
 
-    return (
-      <List disablePadding>
-        <InspectorHeader
-          // TODO: Change name and icon according to the feature type.
-          title={`${values.length}個の建築物`}
-          iconComponent={BuildingIcon}
-          actions={
-            <>
-              <Tooltip title='レイヤーを選択'>
-                <IconButton
-                  aria-label='レイヤーを選択'
-                  onClick={handleSelectLayers}
-                >
-                  <LayerIcon />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title={hidden ? '表示' : '隠す'}>
-                <IconButton
-                  aria-label={hidden ? '表示' : '隠す'}
-                  onClick={hidden ? handleShow : handleHide}
-                >
-                  {hidden ? <VisibilityOffIcon /> : <VisibilityOnIcon />}
-                </IconButton>
-              </Tooltip>
-            </>
-          }
-          onClose={handleClose}
-        />
-        <Divider light />
-        <TileFeaturePropertiesSection values={values} />
-      </List>
-    )
-  }
-)
+  return (
+    <List disablePadding>
+      <InspectorHeader
+        // TODO: Change name and icon according to the feature type.
+        title={`${values.length}個の建築物`}
+        iconComponent={BuildingIcon}
+        actions={
+          <>
+            <Tooltip title='レイヤーを選択'>
+              <IconButton
+                aria-label='レイヤーを選択'
+                onClick={handleSelectLayers}
+              >
+                <LayerIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={hidden ? '表示' : '隠す'}>
+              <IconButton
+                aria-label={hidden ? '表示' : '隠す'}
+                onClick={hidden ? handleShow : handleHide}
+              >
+                {hidden ? <VisibilityOffIcon /> : <VisibilityOnIcon />}
+              </IconButton>
+            </Tooltip>
+          </>
+        }
+        onClose={handleClose}
+      />
+      <Divider light />
+      <TileFeaturePropertiesSection values={values} />
+    </List>
+  )
+}
