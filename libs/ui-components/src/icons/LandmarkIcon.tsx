03995402f907b6555aa7328dd88bc2e9ea2c53e5
Shota Matsuda <shota@takram.com>
2023-07-31 04:22:44 +0900

    Add layer icon components

---
+++ libs/ui-components/src/icons/LandmarkIcon.tsx
@@ -0,0 +1,10 @@
+import { createSvgIcon } from '@mui/material'
+
+export const LandmarkIcon = createSvgIcon(
+  <path
+    fillRule='evenodd'
+    clipRule='evenodd'
+    d='M12 5L12.5 7H17.5V15H12.5L12 13H7.2V19H6V5H12ZM7.2 11.8H12.9369L13.4369 13.8H16.3V8.2H11.5631L11.0631 6.2H7.2V11.8Z'
+  />,
+  'Landmark'
+)
