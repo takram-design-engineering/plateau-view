05c2052e56d01877f140004a96747eabb272f9ed
Shota Matsuda <shota@takram.com>
2023-08-02 14:56:01 +0900

    Add floodRankColorSet

---
+++ libs/datasets/src/colorSets/floodRankColorSet.ts
@@ -0,0 +1,15 @@
+import chroma from 'chroma-js'
+
+import { atomsWithQualitativeColorSet } from '../atomsWithQualitativeColorSet'
+
+export const floodRankColorSet = atomsWithQualitativeColorSet({
+  name: '浸水ランク',
+  colors: [
+    { value: 1, color: chroma.rgb(247, 245, 169).hex(), name: '〜0.5m' },
+    { value: 2, color: chroma.rgb(255, 216, 192).hex(), name: '0.5m〜3m' },
+    { value: 3, color: chroma.rgb(255, 183, 183).hex(), name: '3m〜5m' },
+    { value: 4, color: chroma.rgb(255, 145, 145).hex(), name: '5m〜10m' },
+    { value: 5, color: chroma.rgb(242, 133, 201).hex(), name: '10m〜20m' },
+    { value: 6, color: chroma.rgb(220, 122, 220).hex(), name: '20m〜' }
+  ]
+})
--- libs/datasets/src/colorSets/index.ts
+++ libs/datasets/src/colorSets/index.ts
@@ -1,2 +1,4 @@
+export * from './floodHeightRangeColorSet'
+export * from './floodRankColorSet'
 export * from './landSlideRiskColorSet'
 export * from './landUseColorSet'
