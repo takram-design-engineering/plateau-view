21b9c489b81e851eb4d457410dca2de174d9e9f9
Shota Matsuda <shota@takram.com>
2023-08-08 02:46:38 +0900

    Separate SearchList

--- libs/view/src/overlay/SearchAutocompletePanel.tsx
+++ libs/view/src/overlay/SearchAutocompletePanel.tsx
@@ -1,12 +1,4 @@
-import {
-  Button,
-  ClickAwayListener,
-  Divider,
-  ListItemSecondaryAction,
-  ListSubheader,
-  MenuList,
-  styled
-} from '@mui/material'
+import { ClickAwayListener, Divider, styled } from '@mui/material'
 import { useAtomValue } from 'jotai'
 import {
   useCallback,
@@ -22,63 +14,22 @@ import {
 import { useWindowEvent } from '@takram/plateau-react-helpers'
 import { platformAtom } from '@takram/plateau-shared-states'
 import {
-  AddressIcon,
   AppOverlayLayoutContext,
-  BuildingIcon,
-  DatasetIcon,
-  EntityTitleButton,
   FloatingPanel,
   Scrollable,
   SearchAutocomplete,
   Shortcut,
   testShortcut,
-  type EntityTitleButtonProps,
   type SearchAutocompleteProps,
   type SearchOption
 } from '@takram/plateau-ui-components'
 
 import { useSearchOptions } from '../hooks/useSearchOptions'
+import { SearchList } from './SearchList'
 
 const StyledScrollable = styled(Scrollable)(({ theme }) => ({
   maxHeight: `calc(100% - ${theme.spacing(6)} - 1px)`
 }))
-
-const OptionItem: FC<{
-  iconComponent: EntityTitleButtonProps['iconComponent']
-  option: SearchOption
-  onClick?: (event: MouseEvent, option: SearchOption) => void
-}> = ({ iconComponent, option, onClick }) => {
-  const handleClick = useCallback(
-    (event: MouseEvent) => {
-      onClick?.(event, option)
-    },
-    [option, onClick]
-  )
-  return (
-    <EntityTitleButton
-      iconComponent={iconComponent}
-      title={option.name}
-      onClick={handleClick}
-    />
-  )
-}
-
-const FilterButton: FC<{
-  filter: string
-  onClick?: (event: MouseEvent, filter: string) => void
-}> = ({ filter, onClick }) => {
-  const handleClick = useCallback(
-    (event: MouseEvent) => {
-      onClick?.(event, filter)
-    },
-    [filter, onClick]
-  )
-  return (
-    <Button variant='text' size='small' onClick={handleClick}>
-      絞り込み
-    </Button>
-  )
-}
 
 export interface SearchAutocompletePanelProps {
   children?: ReactNode
@@ -104,12 +55,22 @@ export const SearchAutocompletePanel: FC<SearchAutocompletePanelProps> = ({
     ],
     [searchOptions.datasets, searchOptions.buildings, searchOptions.addresses]
   )
+
   const selectOption = searchOptions.select
+  const handleOptionSelect = useCallback(
+    (event: MouseEvent, option: SearchOption) => {
+      selectOption(option)
+    },
+    [selectOption]
+  )
 
   const [filters, setFilters] = useState<string[]>()
-  const handleClickFilter = useCallback((event: MouseEvent, filter: string) => {
-    setFilters([filter])
-  }, [])
+  const handleFiltersChange = useCallback(
+    (event: MouseEvent, filters: string[]) => {
+      setFilters(filters)
+    },
+    []
+  )
 
   const handleChange: NonNullable<SearchAutocompleteProps['onChange']> =
     useCallback(
@@ -131,13 +92,6 @@ export const SearchAutocompletePanel: FC<SearchAutocompletePanelProps> = ({
       },
       [selectOption]
     )
-
-  const handleClickOption = useCallback(
-    (event: MouseEvent, option: SearchOption) => {
-      selectOption(option)
-    },
-    [selectOption]
-  )
 
   useWindowEvent('keydown', event => {
     // TODO: Manage shortcut globally
@@ -193,73 +147,13 @@ export const SearchAutocompletePanel: FC<SearchAutocompletePanelProps> = ({
             children
           ) : (
             <StyledScrollable>
-              <div>
-                <MenuList component='div' dense>
-                  {searchOptions.datasets.length > 0 && [
-                    <ListSubheader component='div' key='datasets'>
-                      周辺のデータセット
-                      <ListItemSecondaryAction>
-                        <FilterButton
-                          filter='dataset'
-                          onClick={handleClickFilter}
-                        />
-                      </ListItemSecondaryAction>
-                    </ListSubheader>,
-                    ...searchOptions.datasets
-                      .slice(0, 4)
-                      .map((option, index) => (
-                        <OptionItem
-                          key={`datasets:${index}`}
-                          iconComponent={DatasetIcon}
-                          option={option}
-                          onClick={handleClickOption}
-                        />
-                      ))
-                  ]}
-                  {searchOptions.buildings.length > 0 && [
-                    <ListSubheader key='buildings'>
-                      周辺の建築物
-                      <ListItemSecondaryAction>
-                        <FilterButton
-                          filter='building'
-                          onClick={handleClickFilter}
-                        />
-                      </ListItemSecondaryAction>
-                    </ListSubheader>,
-                    ...searchOptions.buildings
-                      .slice(0, 4)
-                      .map((option, index) => (
-                        <OptionItem
-                          key={`buildings:${index}`}
-                          iconComponent={BuildingIcon}
-                          option={option}
-                          onClick={handleClickOption}
-                        />
-                      ))
-                  ]}
-                  {searchOptions.addresses.length > 0 && [
-                    <ListSubheader key='addresses'>
-                      周辺の住所
-                      <ListItemSecondaryAction>
-                        <FilterButton
-                          filter='address'
-                          onClick={handleClickFilter}
-                        />
-                      </ListItemSecondaryAction>
-                    </ListSubheader>,
-                    ...searchOptions.addresses
-                      .slice(0, 4)
-                      .map((option, index) => (
-                        <OptionItem
-                          key={`addresses:${index}`}
-                          iconComponent={AddressIcon}
-                          option={option}
-                          onClick={handleClickOption}
-                        />
-                      ))
-                  ]}
-                </MenuList>
-              </div>
+              <SearchList
+                datasets={searchOptions.datasets}
+                buildings={searchOptions.buildings}
+                addresses={searchOptions.addresses}
+                onOptionSelect={handleOptionSelect}
+                onFiltersChange={handleFiltersChange}
+              />
             </StyledScrollable>
           )}
         </SearchAutocomplete>
