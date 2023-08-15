91362c21d661b2b4a51395205b8a4e291f846fd9
Shota Matsuda <shota@takram.com>
2023-08-15 14:23:50 +0900

    Add tile coordinate props

--- libs/cesium/src/TileCoordinatesImageryLayer.tsx
+++ libs/cesium/src/TileCoordinatesImageryLayer.tsx
@@ -4,21 +4,27 @@ import {
 } from '@cesium/engine'
 import { type FC } from 'react'
 
-import { useConstant } from '@takram/plateau-react-helpers'
-
 import { ImageryLayer, type ImageryLayerProps } from './ImageryLayer'
+import { useInstance } from './useInstance'
 
 export interface TileCoordinatesImageryLayerProps
-  extends Omit<ImageryLayerProps, 'imageryProvider'> {}
+  extends Omit<ImageryLayerProps, 'imageryProvider'>,
+    TileCoordinatesImageryProvider.ConstructorOptions {}
 
 export const TileCoordinatesImageryLayer: FC<
   TileCoordinatesImageryLayerProps
-> = props => {
-  const imageryProvider = useConstant(
-    () =>
+> = ({ tilingScheme, ellipsoid, color, tileWidth, tileHeight, ...props }) => {
+  const imageryProvider = useInstance({
+    keys: [tilingScheme, ellipsoid, color, tileWidth, tileHeight],
+    create: () =>
       new TileCoordinatesImageryProvider({
-        tilingScheme: new WebMercatorTilingScheme()
+        tilingScheme: tilingScheme ?? new WebMercatorTilingScheme(),
+        ellipsoid,
+        color,
+        tileWidth,
+        tileHeight
       })
-  )
+  })
+
   return <ImageryLayer imageryProvider={imageryProvider} {...props} />
 }
