4d6eff45d5ab06edc2d60887eacdb59d1badd556
Shota Matsuda <shota@takram.com>
2023-08-29 01:35:01 +0900

    Add icons

---
+++ libs/ui-components/src/icons/SketchRectangleIcon.tsx
@@ -0,0 +1,10 @@
+import { createSvgIcon } from '@mui/material'
+
+export const SketchRectangleIcon = createSvgIcon(
+  <path
+    fillRule='evenodd'
+    clipRule='evenodd'
+    d='M4 17.3629V6.59961L12 2L20 6.59961V17.3629L12 21.9981L4 17.3629ZM11.3984 11.5828V20.2627L5.2 16.6713V7.99143L11.3984 11.5828ZM12.5984 11.5847V20.2645L18.8 16.6713V7.99145L12.5984 11.5847ZM18.2029 6.95055L12 3.3842L5.79715 6.95054L12 10.5445L18.2029 6.95055Z'
+  />,
+  'SketchRectangle'
+)
