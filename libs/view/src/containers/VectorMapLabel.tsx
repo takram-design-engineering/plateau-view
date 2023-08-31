677d9d2dd184d831ea83d20f45d5f12d08f627c8
Shota Matsuda <shota@takram.com>
2023-08-31 16:30:51 +0900

    Make label component dynamic

--- libs/view/src/containers/VectorMapLabel.tsx
+++ libs/view/src/containers/VectorMapLabel.tsx
@@ -1,11 +1,21 @@
 import { useAtomValue } from 'jotai'
-import { type FC } from 'react'
-
-import { VectorMapLabel } from '@takram/plateau-vector-map-label'
+import dynamic from 'next/dynamic'
+import { Suspense, type FC } from 'react'
 
 import { showMapLabelAtom } from '../states/app'
 
+const VectorMapLabel = dynamic(
+  async () => (await import('@takram/plateau-vector-map-label')).VectorMapLabel,
+  { ssr: false }
+)
+
 export const MapLabel: FC = () => {
   const showMapLabel = useAtomValue(showMapLabelAtom)
-  return showMapLabel && <VectorMapLabel />
+  return (
+    showMapLabel && (
+      <Suspense>
+        <VectorMapLabel />
+      </Suspense>
+    )
+  )
 }
