3a8be06914372ec0660855fa00119ec8349a0d2b
Shota Matsuda <shota@takram.com>
2023-09-04 16:32:54 +0900

    Add measurement text

--- libs/sketch/src/ControlPoints.tsx
+++ libs/sketch/src/ControlPoints.tsx
@@ -1,16 +1,24 @@
 import {
   CallbackProperty,
   Cartesian3,
+  Cartographic,
   ClassificationType,
+  EllipsoidGeodesic,
   HeightReference,
   PolylineDashMaterialProperty,
   type Color,
   type PositionProperty
 } from '@cesium/engine'
+import { styled } from '@mui/material'
 import { memo, useMemo, useRef, type FC } from 'react'
 import invariant from 'tiny-invariant'
 
-import { Entity, useCesium, type EntityProps } from '@takram/plateau-cesium'
+import {
+  Entity,
+  ScreenSpaceElement,
+  useCesium,
+  type EntityProps
+} from '@takram/plateau-cesium'
 import { useConstant } from '@takram/plateau-react-helpers'
 
 import { type GeometryOptions } from './createGeometry'
@@ -26,9 +34,9 @@ function getImage(): HTMLCanvasElement {
   image.height = 16
   const context = image.getContext('2d')
   invariant(context != null)
-  context.fillStyle = '#ffffff'
+  context.fillStyle = 'white'
   context.fillRect(3, 3, 10, 10)
-  context.strokeStyle = '#000000'
+  context.strokeStyle = 'black'
   context.lineWidth = 2
   context.strokeRect(3, 3, 10, 10)
   return image
@@ -104,6 +112,49 @@ const MeasurementLine: FC<{
   return <Entity {...options} />
 }
 
+const MeasurementText = styled('div')(({ theme }) => ({
+  ...theme.typography.caption,
+  padding: theme.spacing(0.5),
+  color: theme.palette.getContrastText(theme.palette.primary.dark),
+  backgroundColor: theme.palette.primary.main,
+  borderRadius: theme.shape.borderRadius,
+  lineHeight: 1
+}))
+
+const cartographicScratch1 = new Cartographic()
+const cartographicScratch2 = new Cartographic()
+
+const Measurement: FC<{
+  a: Cartesian3
+  b: Cartesian3
+  color?: Color
+  showLine?: boolean
+}> = ({ a, b, color, showLine = false }) => {
+  const position = useConstant(() => new Cartesian3())
+  const scene = useCesium(({ scene }) => scene)
+  const geodesic = useMemo(
+    () => new EllipsoidGeodesic(undefined, undefined, scene.globe.ellipsoid),
+    [scene]
+  )
+  geodesic.setEndPoints(
+    Cartographic.fromCartesian(a, scene.globe.ellipsoid, cartographicScratch1),
+    Cartographic.fromCartesian(b, scene.globe.ellipsoid, cartographicScratch2)
+  )
+  const distance = geodesic.surfaceDistance
+  return (
+    <>
+      <ScreenSpaceElement position={Cartesian3.midpoint(a, b, position)}>
+        <MeasurementText>
+          {distance < 1000
+            ? `${distance.toFixed(1)} m`
+            : `${(distance / 1000).toFixed(1)} km`}
+        </MeasurementText>
+      </ScreenSpaceElement>
+      {showLine && <MeasurementLine a={a} b={b} color={color} />}
+    </>
+  )
+}
+
 export interface ControlPointsProps {
   geometryOptions: GeometryOptions
   color?: Color
@@ -116,9 +167,8 @@ export const ControlPoints: FC<ControlPointsProps> = memo(
   ({ geometryOptions: { type, controlPoints: controlPointsProp }, color }) => {
     let controlPoints = controlPointsProp
     let measurementPoints: [Cartesian3, Cartesian3] | undefined
-    if (type === 'circle' && controlPoints.length === 2) {
-      measurementPoints = [controlPoints[0], controlPoints[1]]
-    } else if (type === 'rectangle' && controlPoints.length === 3) {
+    let showLine = false
+    if (type === 'rectangle' && controlPoints.length === 3) {
       const [p1, p2, p3] = controlPoints
       const projection = Cartesian3.projectVector(
         Cartesian3.subtract(p3, p1, cartesianScratch1),
@@ -134,6 +184,10 @@ export const ControlPoints: FC<ControlPointsProps> = memo(
       const p5 = Cartesian3.add(p4, offset, cartesianScratch2)
       controlPoints = [p1, p2, p5]
       measurementPoints = [p4, p5]
+      showLine = true
+    } else if (controlPoints.length > 1) {
+      measurementPoints = controlPoints.slice(-2) as [Cartesian3, Cartesian3]
+      showLine = type === 'circle'
     }
     return (
       <>
@@ -141,10 +195,11 @@ export const ControlPoints: FC<ControlPointsProps> = memo(
           <ControlPoint key={index} position={controlPoint} />
         ))}
         {measurementPoints != null && (
-          <MeasurementLine
+          <Measurement
             a={measurementPoints[0]}
             b={measurementPoints[1]}
             color={color}
+            showLine={showLine}
           />
         )}
       </>
