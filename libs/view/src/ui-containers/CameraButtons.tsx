9d44f4ba333dcbf635f75b85058fd26ee1ce3f19
Shota Matsuda <shota@takram.com>
2023-09-06 03:31:00 +0900

    Add geolocation button

--- libs/view/src/ui-containers/CameraButtons.tsx
+++ libs/view/src/ui-containers/CameraButtons.tsx
@@ -1,6 +1,14 @@
+import { Cartesian3 } from '@cesium/engine'
+import { Button, CircularProgress, Fade } from '@mui/material'
 import { useAtom, type PrimitiveAtom } from 'jotai'
-import { useCallback, type FC } from 'react'
+import { useSnackbar } from 'notistack'
+import { useCallback, useEffect, useState, type FC } from 'react'
 
+import { useCesium } from '@takram/plateau-cesium'
+import {
+  flyToDestination,
+  getCameraEllipsoidIntersection
+} from '@takram/plateau-cesium-helpers'
 import {
   AppIconButton,
   KeyboardMovementIcon,
@@ -27,14 +35,83 @@ function useBooleanAtomProps(atom: PrimitiveAtom<boolean>): {
   return { selected, onClick: handleClick }
 }
 
+const GeolocationButton: FC = () => {
+  const scene = useCesium(({ scene }) => scene, { indirect: true })
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
+  const [enabled, setEnabled] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setEnabled('geolocation' in navigator)
+  }, [])
+
+  const handleClick = useCallback(() => {
+    if (scene == null) {
+      return
+    }
+    setLoading(true)
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        setLoading(false)
+        const target = Cartesian3.fromDegrees(
+          coords.longitude,
+          coords.latitude,
+          undefined,
+          scene.globe.ellipsoid
+        )
+        const cartesian = new Cartesian3()
+        const offset = Cartesian3.subtract(
+          scene.camera.position,
+          getCameraEllipsoidIntersection(scene),
+          cartesian
+        )
+        const position = Cartesian3.add(target, offset, cartesian)
+        flyToDestination(scene, position).catch(error => {
+          console.error(error)
+        })
+      },
+      () => {
+        setLoading(false)
+        const snackbar = enqueueSnackbar({
+          variant: 'error',
+          message: '現在地を取得できませんでした',
+          action: (
+            <Button
+              variant='text'
+              onClick={() => {
+                closeSnackbar(snackbar)
+              }}
+            >
+              閉じる
+            </Button>
+          ),
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'center'
+          },
+          TransitionComponent: Fade
+        })
+      }
+    )
+  }, [scene, enqueueSnackbar, closeSnackbar])
+
+  return (
+    <AppIconButton
+      title='現在地'
+      disabled={!enabled || scene == null || loading}
+      onClick={handleClick}
+    >
+      {loading ? <CircularProgress size={21} /> : <LocationIcon />}
+    </AppIconButton>
+  )
+}
+
 export const CameraButtons: FC = () => {
   const enableKeyboardCameraControlProps = useBooleanAtomProps(
     enableKeyboardCameraControlAtom
   )
   const autoRotateCameraProps = useBooleanAtomProps(autoRotateCameraAtom)
-
   const { zoomIn, zoomOut } = useCameraZoom()
-
   return (
     <>
       <AppIconButton
@@ -43,9 +120,7 @@ export const CameraButtons: FC = () => {
       >
         <KeyboardMovementIcon />
       </AppIconButton>
-      <AppIconButton title='現在地' disabled>
-        <LocationIcon />
-      </AppIconButton>
+      <GeolocationButton />
       <AppIconButton title='自動回転' {...autoRotateCameraProps}>
         <RotateAroundIcon />
       </AppIconButton>
